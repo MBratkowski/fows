8d184b0f957363d4c100567a85d6a59012b2ddfc
MBratkowski <mateusz.bratkowski93@gmail.com>
2016-11-13 14:36:08 +0100

    Continue init project.

--- app/src/main/java/com/fows/activity/PrelegentListActivity.java
+++ app/src/main/java/com/fows/activity/PrelegentListActivity.java
@@ -4,12 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.fows.R;
+import com.fows.activity.base.BaseActivity;
 
-public class PrelegentListActivity extends AppCompatActivity {
+public class PrelegentListActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_prelegent_list);
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_prelegent_list;
     }
 }
