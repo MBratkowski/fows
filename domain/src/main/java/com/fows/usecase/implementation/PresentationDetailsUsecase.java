7bf0a7911453d9833a3ec7f0d4cff4c9f024ccd7
MBratkowski <mateusz.bratkowski93@gmail.com>
2016-11-13 14:34:58 +0100

    Create Usecases.

---
+++ domain/src/main/java/com/fows/usecase/implementation/PresentationDetailsUsecase.java
@@ -0,0 +1,14 @@
+package com.fows.usecase.implementation;
+
+import com.fows.usecase.definition.Usecase;
+
+/**
+ * Created by mateusz.bratkowski on 13/11/16.
+ */
+public class PresentationDetailsUsecase implements Usecase {
+
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ domain/src/main/java/com/fows/usecase/implementation/PresentationListUsecase.java
@@ -0,0 +1,14 @@
+package com.fows.usecase.implementation;
+
+import com.fows.usecase.definition.Usecase;
+
+/**
+ * Created by mateusz.bratkowski on 13/11/16.
+ */
+public class PresentationListUsecase implements Usecase {
+
+    @Override
+    public void execute() {
+
+    }
+}
