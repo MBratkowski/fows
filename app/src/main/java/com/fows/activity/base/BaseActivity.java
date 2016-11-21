06e8c2976bed4099500d192e16f6d4c8653ce04d
MBratkowski <mateusz.bratkowski93@gmail.com>
2016-11-21 17:56:13 +0100

    Refactor BaseActivity with new activity module.

--- app/src/main/java/com/fows/activity/base/BaseActivity.java
+++ app/src/main/java/com/fows/activity/base/BaseActivity.java
@@ -6,8 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.fows.application.FowsApplication;
+import com.fows.application.FowsInjector;
+import com.fows.di.comoponent.ActivityComponent;
 import com.fows.di.comoponent.AppComponent;
+import com.fows.di.comoponent.DaggerActivityComponent;
 import com.fows.di.comoponent.DaggerAppComponent;
+import com.fows.di.module.ActivityModule;
 import com.fows.di.module.AppModule;
 import com.fows.presenter.Presenter;
 import com.fows.view.BaseView;
@@ -24,9 +28,9 @@ public abstract class BaseActivity<P extends Presenter<V>, V extends BaseView> e
     @Inject
     P presenter;
 
-    private AppComponent appComponent;
+    private ActivityComponent activityComponent;
 
-    protected abstract void performFieldInjection(AppComponent appComponent);
+    protected abstract void performFieldInjection(ActivityComponent appComponent);
 
     @LayoutRes
     public abstract int getLayoutId();
@@ -36,7 +40,7 @@ public abstract class BaseActivity<P extends Presenter<V>, V extends BaseView> e
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
         ButterKnife.bind(this);
-        performFieldInjection(getAppComponent());
+        performFieldInjection(getActivityComponent());
         presenter.takeView((V) this);
     }
 
@@ -51,12 +55,16 @@ public abstract class BaseActivity<P extends Presenter<V>, V extends BaseView> e
     }
 
     protected AppComponent getAppComponent() {
-        if (appComponent == null) {
-            appComponent = DaggerAppComponent
-                    .builder()
-                    .appModule(new AppModule(FowsApplication.get(this)))
+        return FowsInjector.INSTANCE.getAppComponent();
+    }
+
+    private ActivityComponent getActivityComponent() {
+        if (activityComponent == null) {
+            activityComponent = DaggerActivityComponent.builder()
+                    .appComponent(getAppComponent())
+                    .activityModule(new ActivityModule(this))
                     .build();
         }
-        return appComponent;
+        return activityComponent;
     }
 }
