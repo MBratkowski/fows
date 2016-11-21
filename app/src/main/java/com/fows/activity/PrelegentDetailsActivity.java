73a4a21cee72aa22c7ca4faefb367a3423fcd657
MBratkowski <mateusz.bratkowski93@gmail.com>
2016-11-21 22:25:42 +0100

    Add definition view in the PrelegentDetailsActivity.

--- app/src/main/java/com/fows/activity/PrelegentDetailsActivity.java
+++ app/src/main/java/com/fows/activity/PrelegentDetailsActivity.java
@@ -1,12 +1,20 @@
 package com.fows.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
+
 import com.fows.R;
 import com.fows.activity.base.BaseActivity;
 import com.fows.di.comoponent.ActivityComponent;
-import com.fows.di.comoponent.AppComponent;
 import com.fows.di.module.PrelegentDetailsModule;
 import com.fows.presenter.PrelegentDetailsPresenter;
 import com.fows.view.PrelegentDetailsView;
+
+import butterknife.BindView;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by mateusz.bratkowski on 20/11/16.
@@ -16,9 +24,47 @@ public class PrelegentDetailsActivity extends BaseActivity<PrelegentDetailsPrese
 
     public static final String EXTRA_PRELGENT_ID = "EXTRA_PRELEGENT_ID";
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.prelegent_image_view)
+    CircleImageView prelegentImageView;
+
+    @BindView(R.id.name_text_view)
+    TextView nameTextView;
+
+    @BindView(R.id.surname_text_view)
+    TextView surnameTextView;
+
+    /*@BindView(R.id.company_text_view)
+    TextView companyTextView;*/
+
+    @BindView(R.id.description_text_view)
+    TextView descriptionTextView;
+
     @Override
     public void showError() {
         //TODO
+    }
+
+    @Override
+    public void displayName(String name) {
+        nameTextView.setText(name);
+    }
+
+    @Override
+    public void displaySurname(String surname) {
+        surnameTextView.setText(surname);
+    }
+
+    @Override
+    public void displayDescription(String descritpion) {
+        descriptionTextView.setText(descritpion);
+    }
+
+    @Override
+    public void displayPhoto(String urlPhoto) {
+
     }
 
     @Override
@@ -32,12 +78,30 @@ public class PrelegentDetailsActivity extends BaseActivity<PrelegentDetailsPrese
     }
 
     @Override
+    public int getLayoutId() {
+        return R.layout.activity_prelegent_details;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     protected void performFieldInjection(ActivityComponent appComponent) {
         appComponent.plus(new PrelegentDetailsModule()).inject(this);
     }
 
     @Override
-    public int getLayoutId() {
-        return R.layout.activity_prelegent_details;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
