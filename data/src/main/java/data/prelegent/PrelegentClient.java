f53cacc4bc6968bd3ecaad796811ae3778b852a9
MBratkowski <mateusz.bratkowski93@gmail.com>
2016-11-28 14:44:04 +0100

    Return Single instead Observable in PrelegentClient class.

--- data/src/main/java/data/prelegent/PrelegentClient.java
+++ data/src/main/java/data/prelegent/PrelegentClient.java
@@ -4,10 +4,12 @@ import com.fows.entity.Prelegent;
 import com.fows.gateway.PrelegentGateway;
 
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import javax.inject.Inject;
 
 import rx.Observable;
+import rx.Single;
 
 /**
  * Created by mateusz.bratkowski on 13/11/16.
@@ -22,12 +24,17 @@ public class PrelegentClient implements PrelegentGateway {
     }
 
     @Override
-    public Observable<List<Prelegent>> getPrelegents() {
-        return Observable.from(provider.getPrelegents()).toList();
+    public Single<List<Prelegent>> getPrelegents() {
+        return Observable.from(provider.getPrelegents()).toList().toSingle();
     }
 
     @Override
-    public Observable<Prelegent> getPrelegent(int prelegentId) {
-        return Observable.just(provider.getPrelegents().get(prelegentId));
+    public Single<Prelegent> getPrelegentDetails(final int prelegentId) {
+        return Observable.fromCallable(new Callable<Prelegent>() {
+            @Override
+            public Prelegent call() throws Exception {
+                return provider.getPrelegents().get(prelegentId);
+            }
+        }).toSingle();
     }
 }
