6f7ddb3a0cafe17cc734833daf9b02cfba68425b
MBratkowski <mateusz.bratkowski93@gmail.com>
2016-12-08 10:25:51 +0100

    Correct typos.

--- ci/copy-unit-result.sh
+++ ci/copy-unit-result.sh
@@ -1,6 +1,6 @@
 mkdir -p $CIRCLE_ARTIFACTS/reports/lint
 mkdir -p $CIRCLE_ARTIFACTS/reports/findbugs
-mkdir -p $CIRCLE_ARTIFCATS/reports/checkstyle
-cp -r app/build/reports/checkstyle/* $CRICLE_ARTIFACTS/reports/checkstyle/
+mkdir -p $CIRCLE_ARTIFACTS/reports/checkstyle
+cp -r app/build/reports/checkstyle/* $CIRCLE_ARTIFACTS/reports/checkstyle/
 cp -r app/build/reports/findbugs/* $CIRCLE_ARTIFACTS/reports/findbugs/
 cp -r app/build/outputs/lint-results* $CIRCLE_ARTIFACTS/reports/lint/
