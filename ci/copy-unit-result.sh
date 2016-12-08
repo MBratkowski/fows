98cf4dbe20a26ede449240c383902160539e79dc
MBratkowski <mateusz.bratkowski93@gmail.com>
2016-12-08 09:34:46 +0100

    Fix bash script - removal unused commands.

--- ci/copy-unit-result.sh
+++ ci/copy-unit-result.sh
@@ -1,11 +1,6 @@
 mkdir -p $CIRCLE_ARTIFACTS/reports/lint
-mkdir -p $CIRCLE_ARTIFACTS/reports/unit_tests
 mkdir -p $CIRCLE_ARTIFACTS/reports/findbugs
 mkdir -p $CIRCLE_ARTIFCATS/reports/checkstyle
-cp -r app/build/test-results/howdyDebug $CIRCLE_TEST_REPORTS/debug
-cp -r app/build/test-results/howdyRelease $CIRCLE_TEST_REPORTS/release
-cp -r app/build/reports/tests/* $CIRCLE_ARTIFACTS/reports/unit_tests/
-cp -r app/build/reports/tests/* $CIRCLE_ARTIFACTS/reports/unit_tests/
-cp -r app/build/reports/checkstyle/* $CRICLE_ARTIFACTS/reports/checkstyle
+cp -r app/build/reports/checkstyle/* $CRICLE_ARTIFACTS/reports/checkstyle/
 cp -r app/build/reports/findbugs/* $CIRCLE_ARTIFACTS/reports/findbugs/
 cp -r app/build/outputs/lint-results* $CIRCLE_ARTIFACTS/reports/lint/
