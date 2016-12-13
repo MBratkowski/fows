7316a3e773f905d5f7b07ee83de2c0f7c75f9ffb
Mateusz Bratkowski <mateusz.bratkowski93@gmail.com>
2016-12-13 13:46:46 +0100

    Bind data in the PrelegentDetailsAdapter.

--- app/src/main/java/com/fows/adapter/PrelegentDetailsAdapter.java
+++ app/src/main/java/com/fows/adapter/PrelegentDetailsAdapter.java
@@ -1,5 +1,6 @@
 package com.fows.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +11,11 @@ import com.fows.R;
 import com.fows.presenter.PrelegentDetailsPresenter;
 import com.fows.view.PrelegentDetailsViewInformationRow;
 import com.fows.view.PrelegentDetailsViewPresentationRow;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by mateusz.bratkowski on 23/11/16.
@@ -21,8 +24,23 @@ public class PrelegentDetailsAdapter extends RecyclerView.Adapter<RecyclerView.V
 
     public static class InformationItem extends RecyclerView.ViewHolder implements PrelegentDetailsViewInformationRow {
 
+        @BindView(R.id.prelegent_image_view)
+        CircleImageView prelgentCircleImageView;
+
         @BindView(R.id.name_text_view)
         TextView nameTextView;
+
+        @BindView(R.id.surname_text_view)
+        TextView surnameTextView;
+
+        @BindView(R.id.company_text_view)
+        TextView companyTextView;
+
+        @BindView(R.id.description_text_view)
+        TextView descriptionTextView;
+
+        @BindView(R.id.prelegent_presentation_header_text_view)
+        TextView prelegentHeaderTextView;
 
         public InformationItem(View itemView) {
             super(itemView);
@@ -36,22 +54,32 @@ public class PrelegentDetailsAdapter extends RecyclerView.Adapter<RecyclerView.V
 
         @Override
         public void displaySurname(String surname) {
-
+            surnameTextView.setText(surname);
         }
 
         @Override
-        public void displayDescription(String descritpion) {
-
+        public void displayDescription(String description) {
+            descriptionTextView.setText(description);
         }
 
         @Override
         public void displayCompany(String company) {
-
+            companyTextView.setText(company);
         }
 
         @Override
         public void displayPhoto(String urlPhoto) {
+            Picasso.with(prelgentCircleImageView.getContext())
+                    .load(urlPhoto)
+                    .error(R.color.colorPrimaryDark)
+                    .into(prelgentCircleImageView);
+        }
 
+        @Override
+        public void displayPrelegentHeader(String name, String surname) {
+            Context context = prelegentHeaderTextView.getContext();
+            String textViewFormatter = context.getString(R.string.item_prelegent_details_information_header_presentation_list);
+            prelegentHeaderTextView.setText(String.format(textViewFormatter, name, surname));
         }
     }
 
@@ -60,29 +88,36 @@ public class PrelegentDetailsAdapter extends RecyclerView.Adapter<RecyclerView.V
         @BindView(R.id.start_time_text_view)
         TextView startTimeTextView;
 
+        @BindView(R.id.day_text_view)
+        TextView dayTextView;
+
+        @BindView(R.id.theme_text_view)
+        TextView themeTextView;
+
         public PresentationItem(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
 
         @Override
-        public void displayStartTimePresentation(String presentationDateTime) {
+        public void displayStartTimePresentation(String startTime) {
+            startTimeTextView.setText(startTime);
+        }
 
+        @Override
+        public void displayDayPresentation(int day) {
+            dayTextView.setText(String.format());
         }
 
         @Override
         public void displayThemePresentation(String presentationOverview) {
-            startTimeTextView.setText(presentationOverview);
+            themeTextView.setText(presentationOverview);
         }
 
-        @Override
-        public void displayAuthorPresntation(String presentationPlace) {
-
-        }
     }
 
     private static final int HEADER_COUNT = 1;
-    private static final int HEADER_POSTIION = 0;
+    private static final int HEADER_POSITION = 0;
 
     private static final int INFORMATION_ITEM_TYPE = 0;
     private static final int PRESENTATION_ITEM_TYPE = 1;
@@ -102,13 +137,14 @@ public class PrelegentDetailsAdapter extends RecyclerView.Adapter<RecyclerView.V
             case PRESENTATION_ITEM_TYPE:
                 return new PresentationItem(LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.item_prelegent_details_presentation, parent, false));
+            default:
+                throw new IllegalArgumentException("Invalid view type");
         }
-        return null;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if (position == HEADER_POSTIION) {
+        if (position == HEADER_POSITION) {
             bindInformation((InformationItem) holder);
         } else {
             bindPresentataion((PresentationItem) holder, position);
@@ -117,7 +153,7 @@ public class PrelegentDetailsAdapter extends RecyclerView.Adapter<RecyclerView.V
 
     @Override
     public int getItemViewType(int position) {
-        return position == HEADER_POSTIION ? INFORMATION_ITEM_TYPE : PRESENTATION_ITEM_TYPE;
+        return position == HEADER_POSITION ? INFORMATION_ITEM_TYPE : PRESENTATION_ITEM_TYPE;
     }
 
     @Override
