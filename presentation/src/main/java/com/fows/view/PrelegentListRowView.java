03750d0493759786949e82959604385746078032
MBratkowski <mateusz.bratkowski93@gmail.com>
2016-12-14 22:09:18 +0100

    Modification arguments set in view interfaces.

--- presentation/src/main/java/com/fows/view/PrelegentListRowView.java
+++ presentation/src/main/java/com/fows/view/PrelegentListRowView.java
@@ -1,11 +1,13 @@
 package com.fows.view;
 
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by mateusz.bratkowski on 20/11/16.
  */
 public interface PrelegentListRowView {
 
-    void displayPhoto(String urlPhoto);
+    void displayPhoto(Picasso picasso, String urlPhoto);
 
     void displayName(String name);
 
