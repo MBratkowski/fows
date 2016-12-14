0d673f4abafba4aafb0e52a9acc5f5fd81eecdf2
MBratkowski <mateusz.bratkowski93@gmail.com>
2016-12-14 22:05:24 +0100

    Set test image.

--- data/src/main/java/data/prelegent/PrelegentProvider.java
+++ data/src/main/java/data/prelegent/PrelegentProvider.java
@@ -13,24 +13,25 @@ public class PrelegentProvider {
 
     private final List<Prelegent> prelegents = new ArrayList<>();
     private final List<Presentation> presentations = new ArrayList<>();
+    private final String imageUrl = "http://i.imgur.com/t8YSfYS.jpg";
 
     public PrelegentProvider() {
         preparePresentationSet();
-        prelegents.add(new Prelegent.Builder(1, "Mateusz", "Bratkowski").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(2, "Karol", "Byra").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(3, "Mateusz", "Chudzinski").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(4, "Marek", "Wesolowski").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(5, "Andrzej", "Chmielewski").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(6, "Ewelina", "Berlicka").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(7, "Bartosz", "Wawrzynczyk").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(8, "Maciek", "Lukasiewicz").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(9, "Kamil", "Cholewa").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(10, "Michal", "Kowalski").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(11, "Aleksandra", "Bratkowska").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(12, "Joanna", "Jakubowska").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(13, "Damian", "Piatek").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(14, "Jacek", "Oko").presentations(presentations).build());
-        prelegents.add(new Prelegent.Builder(15, "Mariusz", "Koziol").presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(1, "Mateusz", "Bratkowski").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(2, "Karol", "Byra").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(3, "Mateusz", "Chudzinski").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(4, "Marek", "Wesolowski").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(5, "Andrzej", "Chmielewski").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(6, "Ewelina", "Berlicka").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(7, "Bartosz", "Wawrzynczyk").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(8, "Maciek", "Lukasiewicz").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(9, "Kamil", "Cholewa").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(10, "Michal", "Kowalski").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(11, "Aleksandra", "Bratkowska").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(12, "Joanna", "Jakubowska").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(13, "Damian", "Piatek").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(14, "Jacek", "Oko").personImage(imageUrl).presentations(presentations).build());
+        prelegents.add(new Prelegent.Builder(15, "Mariusz", "Koziol").personImage(imageUrl).presentations(presentations).build());
     }
 
     public List<Prelegent> getPrelegents() {
